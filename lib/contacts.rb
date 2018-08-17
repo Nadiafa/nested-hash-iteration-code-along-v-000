6666ee61051dff3dd7349d28f2e04a9ffd2fa3b9
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-17 14:25:46 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,8 +15,7 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  ice_cream_arr = contacts["Freddy Mercury"][:favorite_ice_cream_flavors]
-  ice_cream_arr_new = []
+
   ice_cream_arr.each do |i|
     if  i == "strawberry"
     ice_cream_arr_new << ice_cream_arr.delete!(i) 
